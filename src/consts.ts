b39eead6aaefc08ff3bd6174d071e8555998bf94
Jordan <zjzhang325@gmail.com>
2026-01-22 16:17:56 -0800

    save

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = '♠ Balatro Agent Writeup ♣';
+export const SITE_TITLE = 'Balatro Agent';
 export const SITE_DESCRIPTION = 'Building an AI agent to master the poker roguelike';
